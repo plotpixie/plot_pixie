749bbeb7844b5c2f6f74f892c21d780589904f83
Tomas Lin <tomaslin@gmail.com>
2024-03-17 07:56:37 -0700

    Integrate GoRouter for navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
 import 'package:plot_pixie/src/presentation/choice_swiper.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
+final GoRouter _router = GoRouter(
+  routes: <RouteBase>[
+    GoRoute(
+      path: '/',
+      builder: (BuildContext context, GoRouterState state) {
+        return ChoiceSwiper(title: 'Characters');
+      },
+    ),
+  ],
+);
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'Plot Pixie',
+      routerConfig: _router,
       theme: ThemeData(
         primarySwatch: Colors.blue,
         brightness: Brightness.light,
@@ -21,9 +35,6 @@ class MyApp extends StatelessWidget {
       ),
       themeMode: ThemeMode.system,
       // system theme
-      home: ChoiceSwiper(
-        title: '',
-      ),
       debugShowCheckedModeBanner: false,
     );
   }
